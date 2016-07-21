ff393eb0236731d5754786168bc08499c901b98b
rachaelkpope <rachaelkdances@gmail.com>
2016-07-21 10:05:15 -0400

    Update model.rb

--- models/model.rb
+++ models/model.rb
@@ -1,3 +1,14 @@
-def 
-
+class Result
+  def initialize(physical, happy, mental, life_view)
+    @physical=physical
+    @happy=happy
+    @mental=mental
+    @life_view=life_view
+  end
+  def crisis
+    if @mental=="crisis"
+      @result= "please seek help, hotlines etc"
+    end
+  end
+  
 end
